178b862d467c43adfe91b17f9427e0774ba21cb2
Anar Arabli <anar.arabli@gmail.com>
2026-03-03 17:12:32 +0400

    chore(identity): change required code after review and add fixes

--- identity-service/src/main/java/com/wms/identity/entity/Staffs.java
+++ identity-service/src/main/java/com/wms/identity/entity/Staffs.java
@@ -2,21 +2,27 @@ package com.wms.identity.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "staff_profiles")
-@Data
-public class Staffs {
+@Getter
+@Setter
+public class StaffProfile {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     @Column(updatable = false,nullable = false)
     private UUID id;
 
@@ -24,9 +30,9 @@ public class Staffs {
     @JoinColumn(name = "user_id",nullable = false,unique = true)
     private User user;
 
-    @ManyToOne
+
     @JoinColumn(name = "warehouse_id",nullable = false)
-    private Warehouse warehouse;
+    private UUID warehouse;
 
     @Column(name = "position", nullable = false, length = 100)
     private String position;
