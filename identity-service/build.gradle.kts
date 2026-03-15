25fb6eb37dad00c4f1140cc4a57f66009540635f
Ali Ramazanov <toaliramazanov@gmail.com>
2026-03-15 22:36:33 +0400

    style(identity): fix formatting

--- identity-service/build.gradle.kts
+++ identity-service/build.gradle.kts
@@ -8,5 +8,4 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-
 }
